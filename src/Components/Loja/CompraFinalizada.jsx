6f1fa9d394619ed6956cb29d4905a39128f86610
Pedro Santana <pedro.santana9905@gmail.com>
2025-06-26 02:07:11 -0300

    teste demoday

--- src/Components/Loja/CompraFinalizada.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './LojaFinalizada.module.css';
-
-export default function Toast({ mensagem }) {
-  return (
-    <div className={styles.carregamento}>
-      {mensagem}
-    </div>
-  );
-}
