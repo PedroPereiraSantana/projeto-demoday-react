b31193334fa69903406d9c02a1469d70bbaefd0d
Pedro Santana <pedro.santana9905@gmail.com>
2025-06-26 00:10:34 -0300

    atualizando o projeto demoday

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Rout from './routes/AppRouter'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { ProfileProvider } from './contexts/ProfileContext';
+import 'remixicon/fonts/remixicon.css';
+import { SavedArtesProvider } from './contexts/SavedArtesContext';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Rout/>
-  </StrictMode>,
-)
+    <ThemeProvider>
+       <ProfileProvider>
+        <SavedArtesProvider>
+          <RouterProvider router={router} />
+        </SavedArtesProvider>
+      </ProfileProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
